771f1a8dfa77e72c69a072e8f0e660719225e6d2
el5hatib <el5hatib@gmail.com>
2023-07-20 17:46:27 +0300

    Dealing with GetBuilder (as a State Management)

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,10 +1,6 @@
-import 'package:dealing_with_getx/view/pagethree.dart';
-import 'package:dealing_with_getx/view/pagetwo.dart';
+import 'package:dealing_with_getx/controller/homecontroller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'pageone.dart';
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -12,46 +8,45 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HOME'),
+        title: const Text('Home'),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: MaterialButton(
-                onPressed: () {
-                //  Get.to(() => const PageOne()); // Navigate to PageOne view using GetX routing
-                  Get.to(const PageOne());  // Navigate to PageOne view using GetX routing and remove all previous views from stack
-                },
-                color: Colors.blue,
-                child: const Text('Page One'),
-              ),
-              ),
-              const SizedBox(height: 10,),
-              Center(
-                child: MaterialButton(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GetBuilder<HomeController>(
+             init: HomeController(),
+              builder: (controller) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
                   onPressed: () {
-                    Get.to(const PageTwo()); // Navigate to PageTwo view using GetX routing
+                    controller.increment();
                   },
-                  color: Colors.blue,
-                  child: const Text('Page Two'),
+                  icon: const CircleAvatar(child: Icon(Icons.add)),
                 ),
-              ),
-            const SizedBox(height: 10,),
-            Center(
-              child: MaterialButton(
-                onPressed: () {
-                  Get.to(const PageThree()); // Navigate to PageThree view using GetX routing
-                },
-                color: Colors.blue,
-                child: const Text('Page Three'),
-              ),
-            ),
-
-          ],
-        ),
+                const SizedBox(
+                  width: 10,
+                ),
+                 Center(
+                  child: Text(
+                     '${controller.counter}',
+                    style: const TextStyle(fontSize: 30),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                IconButton(
+                  onPressed: () {
+                    controller.decrement();
+                  },
+                  icon: const CircleAvatar(child: Icon(Icons.remove)),
+                ),
+              ],
+            );
+          }),
+        ],
       ),
     );
   }
